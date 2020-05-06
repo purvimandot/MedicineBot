f6790077deb81eaa3a8f64cc289673edd36d116c
Purvi <17ucc044@lnmiit.ac.in>
2020-05-06 15:10:42 +0530

    form added

---
+++ form.js
@@ -0,0 +1,104 @@
+const SurveyCard=require('./survey');
+exports.Form = function() {
+	
+	var s = {
+        "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+        "type": "AdaptiveCard",
+        "version": "1.0",
+        "body": [
+            {   
+                "type": "ColumnSet",
+                "columns": [
+                    {
+                        "type": "Column",
+                        "width": 2,
+                        "items": [
+                            {
+                                "type": "TextBlock",
+                                "text": "Your details",
+                                "weight": "Bolder",
+                                "size": "Medium"
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "body",
+                                "isSubtle": true,
+                                "wrap": true
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "disclaimer",
+                                "isSubtle": true,
+                                "wrap": true,
+                                "size": "Small"
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "Your Name",
+                                "wrap": true
+                            },
+                            {
+                                "type": "Input.Text",
+                                "id": "myName",
+                                "placeholder": "First,Last"
+                            },
+                            {   
+                                "type": "TextBlock",
+                                "text": "Your Email Address",
+                                "wrap": true
+                            },
+                            {
+                                "type": "Input.Text",
+                                "id": "myEmail",
+                                "placeholder": "youremail@example.com",
+                                "style": "Email"
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "Phone Number"
+                            },
+                            {
+                                "type": "Input.Text",
+                                "id": "myTel",
+                                "placeholder": "xxx.xxx.xxx",
+                                "style": "Tel"
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "Gender"
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "Gender"
+                            },
+                            {
+                                "type": "Input.ChoiceSet",
+                                "id": "radio",
+                                "placeholder": "Placeholder text",
+                                "choices": [
+                                    {
+                                        "title": "Male",
+                                        "value": "Choice 1"
+                                    },
+                                    {
+                                        "title": "Female",
+                                        "value": "Choice 2"
+                                    }
+                                ],
+                                "style": "expanded"
+                            }
+                        ]
+                    },
+        
+                ]
+            }
+        ],
+        "actions": [
+            {
+                "type": "Action.Submit",
+                "title": "Submit"
+            }
+        ]
+    };
+    return s;
+}
